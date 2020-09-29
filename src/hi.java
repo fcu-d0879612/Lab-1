b4646fae5a67eeecd1e4de349cd60cf2f5a8b970
user <user@ED07>
2020-09-29 09:32:01 +0800

    hw_1

--- src/hi.java
+++ src/hi.java
@@ -3,9 +3,9 @@ public class hi {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello ");
-		System.out.println("skrrrrr OR");
-		System.out.println("MAOI????");
+		System.out.println("i am So Chin Wai ");
+		System.out.println("my nid is D0879612");
+		System.out.println("this is lab 1 hw skrrrrr");
 	}
 
 }
